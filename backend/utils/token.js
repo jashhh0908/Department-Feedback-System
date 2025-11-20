f36762f42bdf0b6d740355d93510942bee9ca490
jashhh0908 <jashunik9@gmail.com>
2025-11-20 20:37:40 +0530

    fix: use refresh secret key for refresh access token

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -5,7 +5,7 @@ const generateAccessToken = (adminID) => {
 }
 
 const generateRefreshToken = (adminID) => {
-    return jwt.sign({id: adminID}, process.env.JWT_SECRET, {expiresIn: '7d'});
+    return jwt.sign({id: adminID}, process.env.JWT_REFRESH_SECRET, {expiresIn: '7d'});
 }
 
 export {
