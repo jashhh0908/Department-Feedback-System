3f2bea501fc53da966fcc67ecca74226b3d925bc
jashhh0908 <jashunik9@gmail.com>
2025-11-20 19:45:12 +0530

    implemented token generation util and jwt middleware

---
+++ backend/utils/token.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const generateAccessToken = (adminID) => {
+    return jwt.sign({id: adminID}, process.env.JWT_SECRET, {expiresIn: '10m'});
+}
+
+const generateRefreshToken = (adminID) => {
+    return jwt.sign({id: adminID}, process.env.JWT_SECRET, {expiresIn: '7d'});
+}
+
+export {
+    generateAccessToken,
+    generateRefreshToken
+}
