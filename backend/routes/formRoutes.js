82c4d23def5642f349bb8ba1e178f72ddb48d8e7
jashhh0908 <jashunik9@gmail.com>
2025-11-26 23:28:31 +0530

    add submit response function and route

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import { createForm, deactivateForm, deleteForm, getForm, getFormById, reactivateForm, toggleFormStatus, updateForm } from "../controllers/formController.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { authorize } from "../middleware/authorize.middleware.js";
+import { submitResponse } from "../controllers/responseController.js";
 
 const router = express.Router();
 
+//admin-routes
 router.post('/create', verifyToken, authorize("admin"), createForm);
 router.get('/get', verifyToken, authorize("admin"), getForm);
 router.get('/get/:id', verifyToken, authorize("admin"), getFormById);
@@ -13,4 +15,7 @@ router.post('/toggle/:id', verifyToken, authorize("admin"), toggleFormStatus);
 router.post('/deactivate/:id', verifyToken, authorize("admin"), deactivateForm);
 router.post('/reactivate/:id', verifyToken, authorize("admin"), reactivateForm)
 router.delete('/delete/:id', verifyToken, authorize("admin"), deleteForm);
+
+//user-routes
+router.post('/fill-form/:id', verifyToken, authorize("admin", "user"), submitResponse);
 export default router; 
