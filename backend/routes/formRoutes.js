2c143cc388b01bbcda3398c615e9342206aece93
jash_nikumbhe <jashunik9@gmail.com>
2025-12-26 01:52:40 +0530

    include super-admin authorization for all routes

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -3,20 +3,21 @@ import { createForm, deactivateForm, deleteForm, getForm, getFormById, reactivat
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { authorize } from "../middleware/authorize.middleware.js";
 import { getResponses, submitResponse } from "../controllers/responseController.js";
+import { getAnalytics } from "../controllers/analyticsController.js";
 
 const router = express.Router();
 
 //admin-routes
-router.post('/create', verifyToken, authorize("admin"), createForm);
-router.get('/get', verifyToken, authorize("admin"), getForm);
-router.get('/get/:id', verifyToken, authorize("admin"), getFormById);
-router.put('/update/:id', verifyToken, authorize("admin"), updateForm);
-router.post('/toggle/:id', verifyToken, authorize("admin"), toggleFormStatus);
-router.post('/deactivate/:id', verifyToken, authorize("admin"), deactivateForm);
-router.post('/reactivate/:id', verifyToken, authorize("admin"), reactivateForm)
-router.delete('/delete/:id', verifyToken, authorize("admin"), deleteForm);
-router.get('/responses/:id', verifyToken, authorize("admin"), getResponses);    
+router.post('/create', verifyToken, authorize("admin", "super-admin"), createForm);
+router.get('/get', verifyToken, authorize("admin", "super-admin"), getForm);
+router.get('/get/:id', verifyToken, authorize("admin", "super-admin"), getFormById);
+router.put('/update/:id', verifyToken, authorize("admin", "super-admin"), updateForm);
+router.post('/toggle/:id', verifyToken, authorize("admin", "super-admin"), toggleFormStatus);
+router.post('/deactivate/:id', verifyToken, authorize("admin", "super-admin"), deactivateForm);
+router.post('/reactivate/:id', verifyToken, authorize("admin", "super-admin"), reactivateForm)
+router.delete('/delete/:id', verifyToken, authorize("admin", "super-admin"), deleteForm);
+router.get('/responses/:id', verifyToken, authorize("admin", "super-admin"), getResponses);    
     
 //user-routes
-router.post('/fill-form/:id', verifyToken, authorize("admin", "user"), submitResponse);
+router.post('/fill-form/:id', verifyToken, authorize("admin", "super-admin", "user"), submitResponse);
 export default router; 
