a85cbbf5652192f6ae7f83569bd94a428ad5f711
jash_nikumbhe <jashunik9@gmail.com>
2025-12-26 21:33:20 +0530

    setup cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 import connectDB from './config/connectDB.js';
 import authRoutes from './routes/authRoutes.js';
 import formRoutes from './routes/formRoutes.js';
@@ -12,6 +12,10 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.use(cookieParser())
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/form', formRoutes);
